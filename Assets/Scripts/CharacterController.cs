2efd4b21d5ac9cb3ac2cf8b51d57512a97487c68
Andriy Massimilla <andriymassimilla@gmail.com>
2017-08-07 18:07:30 -0400

    Miscellaneous changes

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float speed = 3;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate() {
+        float moveX = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+    }
+}
