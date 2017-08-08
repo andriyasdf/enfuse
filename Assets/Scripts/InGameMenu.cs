c1244ccbba44533cd65452d417a8a56f367c5191
Andriy Massimilla <andriymassimilla@gmail.com>
2017-08-08 17:28:09 -0400

    Pause menu

---
+++ Assets/Scripts/InGameMenu.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMenu : MonoBehaviour {
+
+	public GameObject pauseMenu;
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Escape)) {
+			if (pauseMenu.activeSelf) {
+				ResumeGame();
+			} else {
+				PauseGame();
+			}
+		}
+	}
+
+	public void PauseGame() {
+		Time.timeScale = 0;
+		Debug.Log("Pause!");
+
+		// Enable pause menu
+		pauseMenu.SetActive(true);
+	}
+
+	public void ResumeGame() {
+		Time.timeScale = 1.0f;
+
+		// Disable pause menu
+		pauseMenu.SetActive(false);
+	}
+
+	public void QuitToMenu() {
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
